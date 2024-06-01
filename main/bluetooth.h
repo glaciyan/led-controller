5b2d10f9c874b91126fbf83f8d5bcc9aa9bec95e
Kevin T <30321316+glaciyan@users.noreply.github.com>
2024-06-01 17:16:35 +0200

    blue

--- main/bluetooth.h
+++ main/bluetooth.h
@@ -3,19 +3,12 @@
 #include "esp_log.h"
 #include "esp_err.h"
 
-#include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
-#include "host/ble_hs.h"
-#include "services/gap/ble_svc_gap.h"
-#include "services/gatt/ble_svc_gatt.h"
-#include "services/ans/ble_svc_ans.h"
-
 namespace bluetooth
 {
     static const char *TAG = "bluetooth";
 
     constexpr void init_ble()
     {
-        ESP_LOGI(TAG, "Initializing NimBLE");
+        ESP_LOGI(TAG, "Initializing BLE");
     }
 }
